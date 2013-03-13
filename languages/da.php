7320f99679fca420dda4bd9d1f52ff1b250c476f
cmb69 <cmbecker69@gmx.de>
2013-03-13 20:53:24 +0000

    added Danish language file (contributed by maeg)

---
+++ languages/da.php
@@ -0,0 +1,29 @@
+<?php
+
+$plugin_tx['extedit']['mode_edit']="Rediger";
+$plugin_tx['extedit']['mode_view']="Vis";
+
+$plugin_tx['extedit']['err_changed']="Indholdet af '%s' blev ændret i mellemtiden! Gem igen for at overskrive de tidligere ændringer.";
+
+$plugin_tx['extedit']['imagepicker_title']="Vælg et billede";
+$plugin_tx['extedit']['imagepicker_empty']="Der er ingen billeder i biledemappen.";
+
+$plugin_tx['extedit']['synopsis']="Synopsis";
+$plugin_tx['extedit']['synopsis_username']="BRUGERNAVN";
+$plugin_tx['extedit']['synopsis_username_desc']="Navnet på Register_XH eller Memberpages_XH brugeren, der må rette indholdet.";
+$plugin_tx['extedit']['synopsis_textname']="TEKSTNAVN";
+$plugin_tx['extedit']['synopsis_textname_desc']="Navnet på extedit. Navnet anvendes som Heading på siden.";
+
+$plugin_tx['extedit']['syscheck']="System check";
+$plugin_tx['extedit']['syscheck_phpversion']="PHP version &ge; %s";
+$plugin_tx['extedit']['syscheck_extension']="Extension '%s' loaded";
+$plugin_tx['extedit']['syscheck_encoding']="Encoding 'UTF-8' configured";
+$plugin_tx['extedit']['syscheck_magic_quotes']="'Magic_quotes_runtime' off";
+$plugin_tx['extedit']['syscheck_writable']="Folder '%s' writable";
+
+$plugin_tx['extedit']['about']="About Extedit_XH";
+
+$plugin_tx['extedit']['cf_images_subfolder']="Hvis editoeren skal have adgang til den normale billedemappe (\"0\") eller sin egen undermappe (\"1\").";
+$plugin_tx['extedit']['cf_allow_scripting']="Tillad at bruge pliuins i exiedit:xh': \"1\" (ja) eller \"0\" (nej).";
+
+?>
