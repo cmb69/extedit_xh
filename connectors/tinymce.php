37b9d1506c7b0777608cb40a85e076fe894b36e8
cmb69 <cmbecker69@gmx.de>
2014-10-09 21:41:14 +0000

    added connectors/tinymce.php to checked PHP sources

--- connectors/tinymce.php
+++ connectors/tinymce.php
@@ -1,6 +1,25 @@
 <?php
 
-function extedit_tinymce_init()
+/**
+ * The tinyMCE connector of Extedit_XH.
+ *
+ * PHP versions 4 and 5
+ *
+ * @category  CMSimple_XH
+ * @package   Extedit
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2013-2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Extedit_XH
+ */
+
+/**
+ * Returns the JavaScript required for the imagepicker.
+ *
+ * @return string
+ */
+function Extedit_Tinymce_init()
 {
     if (session_id() == '') {
         session_start();
