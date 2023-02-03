33453ea98c9f3313a405b7a14751e8558ef4be5c
Christoph M. Becker <cmbecker69@gmx.de>
2023-02-03 01:10:34 +0100

    Close #23 (Require PHP ≥ 7.1.0)

--- classes/Controller.php
+++ classes/Controller.php
@@ -105,7 +105,7 @@ class Controller extends AbstractController
         global $pth, $tx, $plugin_tx;
 
         $ptx = $plugin_tx['extedit'];
-        $phpVersion = '5.3.0';
+        $phpVersion = '7.1.0';
         $checks = array();
         $checks[sprintf($ptx['syscheck_phpversion'], $phpVersion)]
             = version_compare(PHP_VERSION, $phpVersion) >= 0 ? 'ok' : 'fail';
